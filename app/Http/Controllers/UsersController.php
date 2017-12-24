55f11c5091ad6b4b79bb3e31f4166016ff114cb4
hanfeng_hs <linkhanfeng@gmail.com>
2017-12-24 15:44:47 +0000

    call it a night 2017-12-24 23:44:44

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,21 +10,21 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        // 不需要验证可以访问的方法
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store'] // 排除不需要验证的方法
+        ]);
+        // 只让未登录用户访问注册页面：
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     // 注册表单
     public function create()
     {
         return view('users.create');
-    }
-
-    /**
-     * 显示用户个人信息
-     * @param  User   $user 声明一个 Eloquent 类型的变量 $user, 并且与 URI 中的变量 {user} 相对应
-     * @return [type]       视图
-     */
-    public function show(User $user)
-    {
-        // dd(compact('user'));
-        return view('users.show', compact('user'));
     }
 
     /**
@@ -48,7 +48,49 @@ class UsersController extends Controller
         // 注册成功后自动登录 方法
         Auth::login($user);
 
-        session()->flash('success', trans('user_register_success'));
+        session()->flash('success', trans('msg.user_register_success'));
         return redirect()->route('users.show', [$user->id]); // === return redirect()->route('users.show', [$user->id]);
+    }
+
+    /**
+     * 显示用户个人信息
+     * @param  User   $user 声明一个 Eloquent 类型的变量 $user, 并且与 URI 中的变量 {user} 相对应
+     * @return [type]       视图
+     */
+    public function show(User $user)
+    {
+        // dd(compact('user'));
+        return view('users.show', compact('user'));
+    }
+
+    /**
+     * 显示编辑表单
+     * @param  User   $user [隐形路由]
+     */
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', trans('msg.user_update_success'));
+        return redirect()->route('users.show', $user->id);
     }
 }
--- resources/lang/zh-CN/msg.php
+++ resources/lang/zh-CN/msg.php
@@ -4,4 +4,6 @@ return [
     'user_login_success' => '欢迎回来, :username',
     'user_register_success' => '欢迎, 注册成功. 您将在这里开启一段新的旅程~',
     'user_logout_success' => '您已成功登出!',
+    'user_update_success' => '您的个人信息更改成功!',
+    'your_are_logined' => '您已登录，无需再次操作。',
 ];
